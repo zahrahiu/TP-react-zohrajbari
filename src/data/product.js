3256b5a4073dce3fed73bddf86ecbbebad10bca8
zahra <zohrajbari2003@gmail.com>
2025-05-25 18:44:02 +0100

    add local images for products

---
+++ src/data/product.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: 1,
+    name: "1 Million",
+    price: "950 MAD",
+    description: "Parfum chaud et épicé.",
+    image: "/assets/1 million.png",
+  },
+  {
+    id: 2,
+    name: "Bleu de Chanel",
+    price: "1200 MAD",
+    description: "Parfum frais et boisé.",
+    image: "/assets/bleu de chanel.png",
+  },
+];
+
+export default products;
